2ada0b003469d5ba49faba1365a248fffc4c9d8f
AlecBold <sanyokboss55@gmail.com>
2019-12-08 19:40:03 +0300

    some changes

--- model/get_files.py
+++ model/get_files.py
@@ -3,10 +3,8 @@ from os.path import isfile, join, curdir
 import numpy as np
 import json
 
-
-path_user_images = f'{curdir}/media/user_images'
 path_data_images = f'{curdir}/media/images'
-path_to_json_file = f'{curdir}/json/vectors_and_file_names.json'
+path_to_json_file = f'{curdir}/json_file/vectors_and_file_names.json'
 
 
 def get_names_of_images():
@@ -14,12 +12,7 @@ def get_names_of_images():
     return files
 
 
-def get_binary_user_file():
-    # Binary file
-    return 0
-
-
-def get_url_images(file_names):
+def get_path_images(file_names):
     url_images = [join(path_data_images, name) for name in file_names]
     return url_images
 
@@ -39,6 +32,7 @@ def json_file_exist():
     except FileNotFoundError:
         return False
 
+
 def make_json_file(names, vectors):
     names_and_vectors = {}
     for i in range(len(vectors)):
@@ -47,10 +41,8 @@ def make_json_file(names, vectors):
         json.dump(names_and_vectors, file)
 
 
-
 def get_predicted_vectors_from_json():
     with open(path_to_json_file) as file:
         names, vectors = zip(*json.load(file).items())
         vectors = [unjsonify(vector) for vector in vectors]
         return names, vectors
-      
